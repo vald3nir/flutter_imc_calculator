42dd88ebbd11c5028dd0892f97081768ab08c873
Valdenir Severino <vald3nir@gmail.com>
2021-11-17 20:44:19 -0300

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_imc_calculator/src/views/app/app_widget.dart';
+
+import 'src/views/home/home_page.dart';
 
 void main(List<String> args) {
   runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
