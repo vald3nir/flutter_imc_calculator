750ee02902bc96f6dcd338863e8a65e1f754275c
vald3nir <vald3nir@gmail.com>
2021-12-18 14:31:11 -0300

    add windows platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'src/views/home/home_page.dart';
--- lib/src/views/home/home_page.dart
+++ lib/src/views/home/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_imc_calculator/src/views/home/home_viewmodel.dart';
 
